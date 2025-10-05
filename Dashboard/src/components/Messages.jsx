14de97d7e1189b9850425e905a4eb6b6d1c287cf
Ritika Parashar <sadhnakk1212@gmail.com>
2025-10-05 23:01:18 +0530

    Update Messages.jsx

--- Dashboard/src/components/Messages.jsx
+++ Dashboard/src/components/Messages.jsx
@@ -11,7 +11,7 @@ const Messages = () => {
     const fetchMessages = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4000/api/v1/message/getall",
+          "https://hospital-management-system-9-j7sq.onrender.com/api/v1/message/getall",
           { withCredentials: true }
         );
         setMessages(data.messages);
